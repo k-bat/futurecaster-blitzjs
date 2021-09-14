18bd0ada93510cacd47500412fb6e785457014a0
k-bat <kadeem.young8290@gmail.com>
2021-09-13 20:35:32 -0400

    feat: getFuturecastSegment
    and more input content

--- app/futurecasts/mutations/createFuturecast.ts
+++ app/futurecasts/mutations/createFuturecast.ts
@@ -3,51 +3,233 @@ import db from "db"
 
 const defaultFortuneMainText = "The future is not yet clear..."
 
-const whens = ["soon", "never", "it already happen that"]
-const whenModifers = ["will", `won't`]
-const subjects = ["you", "a cat somewhere", "all of the people", "fred"]
-const verbs = ["eat", "outrace", "jump up and down with"]
-const nouns = [
-  "a hippo",
-  "a cat",
-  "a dog",
-  "sodas",
-  "bagels",
-  "a single drop of coffee",
-  "a potion",
-  "a nifty hat",
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
+const timesToBeUpon = [
+  "a time",
+  `a midsummer's eve`,
+  "...um...like earlier today...",
+  "a fortine",
+  ["the ides of ", months],
+]
+const subjectModifiers = ["will", `won't`]
+
+// normalNouns plural form works like a {normalNoun} and many {normalNoun}s
+const normalNouns = [
+  "face",
+  "hippo",
+  "cat",
+  "dog",
+  "bagel",
+  "latte",
+  "potion",
+  "hat",
+  "pet",
   "joy",
-  "nirvana",
-  "that laffy taffy",
-  "actual depression",
+  "genie",
+  "pickle",
+  "hole",
+  "ogre",
+  "banana",
+  "vegetable",
+  "bro",
+  "dude",
+  "zombie",
+  "vampire",
+]
+const emojis = [
   "🔥",
   "📙",
   "😎",
   "🧛",
+  "❤️",
+  "💩",
+  "🌈",
+  "☕",
+  "🙌",
+  "😂",
+  "😭",
+  "👏",
+  "👌",
+  "🔮",
+  "🧙",
+  "⭐",
+  "🌟",
+  "🌞",
+  "🌙",
+  "🌕",
+  "🌚",
+  "😝",
+  "😜",
 ]
-const adjectives = ["friendly", "ugly", "stinky", "totes neato", "chubby", "short", "fuerte"]
+const specialNouns = [
+  "angelology",
+  "demonology",
+  "Etemenanki, the Tower of Babel",
+  "elves",
+  "some peeps from a legit bougie foreign country",
+  "the science",
+  "dirty Texan Republicans",
+  "gun control advocates",
+  "the liberal agenda",
+  "fae",
+  "you",
+  "nirvana",
+  "daddy",
+  "mommy",
+  "literally everyone",
+  "two dudes (gender neutral dudes, could be ladies) from that one party",
+  "that laffy taffy",
+  "actual depression",
+  "can of soda",
+  "ur mom lol",
+  "sarah",
+  "john",
+  "urself",
+  "celebrity",
+  "some rando",
+  `the most cursed of all social media platforms, whose name shall not be spoken,`,
+  "TikTok",
+  emojis,
+]
+const nouns = [normalNouns, specialNouns]
+
+const verbs = [
+  "eat",
+  "run",
+  "do the secret handshake",
+  "jump up and down",
+  "dance",
+  "karate chop",
+  "tickle",
+  "tell a joke",
+  "play ball",
+  "score the points",
+  "swim",
+  "get swole",
+  "learn the things",
+  "become litty",
+  "find meaning",
+  ["find", specialNouns],
+]
+
+const introductions = [
+  "wise man say",
+  ["wise", normalNouns, "say"],
+  "according to science,",
+  "i tell u wat, ",
+  ["once upon", timesToBeUpon],
+]
+
+const times = [
+  "almost",
+  "sometimes",
+  "always",
+  "soon",
+  "never",
+  ", as previously noted,",
+  "often",
+  "from here on out",
+  ["until the", normalNouns, verbs],
+  "now",
+]
+const adjectives = [
+  "black",
+  "bottomless",
+  "curvy",
+  "sensual",
+  "nasty tasting",
+  "cage-free",
+  "open source",
+  "cool sounding",
+  "dumb smelling",
+  "dumb looking",
+  "super technologically advanced",
+  "12th century",
+  "magic-y",
+  "friendly",
+  "ugly",
+  "stinky",
+  "totes neato",
+  "chubby",
+  "short",
+  "fuerte",
+  "noice",
+  "GigaChad",
+  "nifty",
+  "a single drop of",
+]
 const conjunctions = ["and", "but", "therefore"]
-const quips = [`just don't`, `bruh`, `ye shall, my dawg`, `yeet`, `Ayyyy!`]
+const endings = [
+  `just don't`,
+  `bless your heart`,
+  `yasssss queen!`,
+  `mk girlfriend?`,
+  `this could be us but u playin`,
+  `bruh`,
+  `ye shall, my dawg`,
+  "turn up!",
+  "just let the energy vibe ur waves, bro",
+  `yeet`,
+  `Ayyyy!`,
+  `ISN'T THAT SUPER AWESOME?`,
+]
+const hashtags = ["#bet", "#bigfacts", "#idc", "#iykyk", "#allthattea", "#sayless"]
 
 // TODO: move to seeders
 const FORTUNE_TEMPLATES = [
-  [whens, subjects, whenModifers, verbs, nouns],
-  [verbs, nouns, quips],
-  [quips],
+  [introductions, nouns, subjectModifiers, verbs, nouns],
+  [introductions, nouns, subjectModifiers, verbs, nouns, conjunctions, verbs, nouns],
+  [nouns, subjectModifiers, times, verbs, nouns],
+  [nouns, subjectModifiers, times, verbs, conjunctions, nouns, subjectModifiers, verbs],
+  [nouns, subjectModifiers, times, verbs, conjunctions, nouns, subjectModifiers, verbs, nouns],
+  [`aint no`, nouns, `like a`, nouns],
+  [verbs, nouns, endings],
+  [endings],
+  [adjectives, adjectives, nouns, subjectModifiers, verbs, hashtags, hashtags],
+  [nouns, subjectModifiers, verbs, endings],
+  [endings, hashtags],
+  [emojis, hashtags],
+  [emojis, emojis, emojis],
+  [hashtags],
 ]
+
+const defaultPieceText = "idk"
+type TemplatePiece = string | string[] | TemplatePiece[] | undefined
+
+const getFuturecastSegment = (tp: TemplatePiece, uuidSeed: number): string => {
+  if (!tp) {
+    return defaultPieceText
+  } else if (typeof tp === "string") {
+    return `${tp} `
+  }
+
+  const nextTp = tp[uuidSeed % tp.length]
+  return getFuturecastSegment(nextTp, uuidSeed)
+}
 
 const getFuturecastByCastToken = (uuid: string): string => {
   const currFortuneTemplateIndex = uuid.charCodeAt(0) % FORTUNE_TEMPLATES.length
-  const currFortuneTemplate = FORTUNE_TEMPLATES[currFortuneTemplateIndex] as string[][]
-  const defaultPieceText = "idk"
+  const currFortuneTemplate = FORTUNE_TEMPLATES[currFortuneTemplateIndex] || []
   let fortuneText = ""
 
   for (let i = 0; i < currFortuneTemplate.length; i++) {
-    const currTemplatePiece = currFortuneTemplate[i] as string[]
     const uuidSeedIndex = i % uuid.length
     const uuidSeed = uuid.charCodeAt(uuidSeedIndex)
-    const selectedPiece = currTemplatePiece[uuidSeed % currTemplatePiece.length] || defaultPieceText
-    fortuneText = `${fortuneText} ${selectedPiece}`
+    const currTemplatePiece: TemplatePiece = currFortuneTemplate[i]
+    fortuneText += getFuturecastSegment(currTemplatePiece, uuidSeed)
   }
 
   return fortuneText.trim()
